77bb321f075736f1794731bbe74f8f3d305ea6ba
shukawam <shukawam@gmail.com>
2023-05-10 01:51:43 +0900

    Fix provider source

---
+++ README.md
@@ -0,0 +1,3 @@
+# terraform-oci-oke-quickstart
+
+Terraform module for OCI Tutorials(let's create oke cluster.)
--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=1.4.5"
   required_providers {
     oci = {
-      source  = "registry.terraform.io/hashicorp/oci"
+      source  = "oracle/oci"
       version = ">= 4.116.0, < 5.0.0"
     }
   }
