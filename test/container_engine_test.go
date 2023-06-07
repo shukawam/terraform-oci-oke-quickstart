6cec6784bf0eb01c0a1b53a3bb3e242b7c9b1598
shukawam <shukawam@gmail.com>
2023-06-07 18:27:31 +0900

    Fixed e2e test

--- test/container_engine_test.go
+++ test/container_engine_test.go
@@ -12,9 +12,17 @@ func TestContainerEngine(t *testing.T) {
 
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../",
-		VarFiles: []string{
-			"variables.tfvars",
+		Vars: map[string]interface{}{
+			"region":                                    "uk-london-1",
+			"vcn_display_name":                          "example-vcn",
+			"cluster_name":                              "example-cluster",
+			"node_pool_instance_number":                 "1",
+			"node_pool_node_shape_config_ocpus":         "1",
+			"node_pool_node_shape_config_memory_in_gbs": "4",
 		},
+		// VarFiles: []string{
+		// 	"variables.tfvars",
+		// },
 		NoColor: false,
 	})
 
